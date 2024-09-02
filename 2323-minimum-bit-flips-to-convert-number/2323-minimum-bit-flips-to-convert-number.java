5b8bd04cdc5953665d173572885681092b752e99
Dishani Ghosh <109428534+Dishani-Ghosh@users.noreply.github.com>
2024-09-02 10:18:27 +0530

    Time: 0 ms (100%), Space: 40.2 MB (90.86%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
@@ -1,14 +1,12 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        int ans=(start^goal);
+        int ans= (start^goal);//differnce ex: 1010^0111=1101
         int cnt=0;
-        for(int i=0;i<32;i++)//int =32 bit
+        for(int i=0;i<32;i++)//bcz i is integer 32 bit
         {
-            if((ans & (1<<i)) !=0)
-            {
-                cnt++;
-            }
+            if((ans & (1<<i))!=0)//check ith bit is set or not
+            cnt++;
         }
         return cnt;
-    }
+            }
 }
