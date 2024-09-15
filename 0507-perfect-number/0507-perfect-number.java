8f00eda545d534adbf4093ff5fcbc84a7d065c60
Dishani Ghosh <109428534+Dishani-Ghosh@users.noreply.github.com>
2024-09-15 19:05:04 +0530

    Time: 1055 ms (74.95%), Space: 40.2 MB (74.24%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum=0;
+        int n=num/2;
+     for(int i=1;i<=n;i++)
+     {
+        if(num%i==0)
+        sum=sum+i;
+     }   
+     if(sum==num)
+     return true;
+    return false;
+    }
+}
