dec5e4d75c27af862de6f9c1ea870e9d3d15f07d
Dishani Ghosh <109428534+Dishani-Ghosh@users.noreply.github.com>
2024-09-19 21:28:01 +0530

    Time: 5 ms (98.15%), Space: 43.5 MB (36.16%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -6,10 +6,11 @@ class Solution {
             arr[i]=String.valueOf(nums[i]);//it convert integer value to string
 
         }
-        Arrays.sort(arr,(a,b)->(b+a).compareTo(a+b));
-        
+        Arrays.sort(arr,(a,b)->(b+a).compareTo(a+b));//(30+3(b+a)=303 as a )compareTo(3+30=330(a+b) as b)
+        //if a less than b then return -1
+        //if b less than a then return 1|| and if equal= 0
         if(arr[0].equals("0"))
-        return "0";
+        return "0";//if first element is 0 then we take as 0 as a string dont skip it
         StringBuilder sb=new StringBuilder();
         for(String i:arr)
         {
