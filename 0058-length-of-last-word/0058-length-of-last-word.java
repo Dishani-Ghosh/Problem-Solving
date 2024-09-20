157c5b26535239d9b11ff1da6e143d4c3ceee1fc
Dishani Ghosh <109428534+Dishani-Ghosh@users.noreply.github.com>
2024-09-20 23:54:32 +0530

    Time: 0 ms (100%), Space: 41.9 MB (20.24%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -14,3 +14,12 @@ class Solution {
      return count;
     }
 }
+
+/*
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] a = s.split(" ");
+        int n = a.length;
+        return a[n-1].length();
+    }
+}*/
