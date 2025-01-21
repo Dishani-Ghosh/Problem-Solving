a4f3ec3b592bf466c1210977584adbb5efcb790c
Dishani Ghosh <109428534+Dishani-Ghosh@users.noreply.github.com>
2025-01-21 12:03:04 +0530

    Time: 2 ms (98.77%), Space: 42.2 MB (66.28%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -1,26 +1,27 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        StringBuilder sb=new StringBuilder();
-        int depth=0;
-        for(char c:s.toCharArray())
+      StringBuilder sb=new StringBuilder();
+      int depth=0;
+      char ch[]=s.toCharArray();
+      for(char c:ch)
+      {
+        if(c=='(')
         {
-            if(c=='(')
+            if(depth>0)
             {
-                if(depth>0)// if depth is not 0 so it is not first element so it add in sb
-                {
-                    sb.append(c);
-                }
-            depth++;// skip if outer
+                sb.append(c);
             }
-            else if(c==')')
+            depth++;
+        }
+        if(c==')')
+        {
+            depth--;
+            if(depth>0)
             {
-                depth--;//it decreases so if depth==0 that's means it is outer parenthesis
-                if(depth>0)// it it > 0 then we can add it
-                {
-                    sb.append(c);
-                }
+                sb.append(c);
             }
         }
-        return sb.toString();
+      }
+      return sb.toString();  
     }
 }
